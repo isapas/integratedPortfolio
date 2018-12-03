e5f07f04eafdec1468c36c1c316bdc78c2d7e1a1
isapas <isabelle.passeniers@gmail.com>
2018-12-03 07:19:45 +0100

    create files form back office, templates header and footer

---
+++ template/footer.php
@@ -0,0 +1,70 @@
+</main>
+				<!-- start footer Area -->		
+				<footer class="footer-area section-gap">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4 col-md-5 col-sm-6">
+								<div class="single-footer-widget text-white">
+									<h6>A propos</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+									</p>
+									<p class="footer-text">
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+										Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+										<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+									</p>	
+								</div>
+							</div>
+							<div class="col-lg-5 col-md-5 col-sm-6">
+								<form id="contact" class="form-vertical">
+									<div class="form-group">
+										<label class="text-white" for="name">Name</label>
+										<input type="text" class="form-control" id="name" placeholder="Nom">
+									</div>
+									<div class="form-group">
+										<label  class="text-white"for="email">Email</label>
+										<input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+									</div>
+									<div class="form-group ">
+										<label class="text-white" for="message">Votre message</label>
+									<textarea  class="form-control" placeholder=""></textarea> 
+									</div>
+									<button type="submit" class="btn btn-default">envoyer</button>
+								</form>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 social-widget">
+								<div class="single-footer-widget mb-0 text-white">
+									<h6 class="">Suivez moi</h6>
+									<p></p>
+									<div class="footer-social d-flex align-items-center justify-content-around">
+											<a href="https://www.linkedin.com/in/isabelle-passeniers/"><i class="fab fa-linkedin fa-2x"></i></a>
+											<a href="https://twitter.com/isapass"><i class="fab  fa-twitter-square fa-2x"></i></a>
+											<a href="https://github.com/isapas"><i class="fab fa-github fa-2x"></i></a>
+									</div>
+								</div>
+							</div>	
+							</div>
+					</div>
+
+				</footer>	
+				<!-- End footer Area -->					
+			</div>
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>			
+			<script src="js/jquery.sticky.js"></script>			
+			<script src="js/jquery.nice-select.min.js"></script>			
+			<script src="js/parallax.min.js"></script>	
+			<script src="js/mail-script.js"></script>
+			<script src="js/isotope.pkgd.min.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
+
+
+
---
+++ template/header.php
@@ -0,0 +1,80 @@
+
+	<!DOCTYPE html>
+	<html lang="fr" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="Isabelle Passeniers">
+		<!-- Meta Description -->
+		<meta name="description" content="portfolio d'Isabelle Passeniers">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="Isabelle Passeniers">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Portfolio</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">					
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+			 		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+			 		crossorigin="anonymous">
+			<script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" 
+					integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" 
+					crossorigin="anonymous">
+			</script>
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" 
+					integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" 
+					crossorigin="anonymous">
+		</head>
+		<body>
+			<div class="protfolio-wrap">
+
+			<!-- Start Header Area -->
+			<header class="default-header">
+				<nav class="navbar navbar-expand-lg  navbar-light">
+					<div class="container">
+						  <a class="navbar-brand" href="index.html">
+						  	<img src="./Portfolio-Doc/img/balisesourislogo.png" alt="logo">
+						  </a>
+						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="text-white lnr lnr-menu"></span>
+						  </button>
+
+						  <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+						    <ul class="navbar-nav">
+								<li><a href="#Accueil">Accueil</a></li>
+								<li><a href="#portfolio">Portfiolio</a></li>
+								<li><a href="#parcours">Parcours</a></li>
+								<li><a href="#contact">Contact</a></li>
+							   <!-- Dropdown 
+							    <li class="dropdown">
+							      <a class="dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+							        Pages
+							      </a>
+							      <div class="dropdown-menu">
+							        <a class="dropdown-item" href="generic.html">Generic</a>
+							        <a class="dropdown-item" href="elements.html">Elements</a>
+							      </div>
+									</li>	 
+								-->
+						    </ul>
+						  </div>						
+					</div>
+				</nav>
+			</header>
+			<!-- End Header Area -->	
+	<main>
